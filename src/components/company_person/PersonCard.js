c13b846ab767111d8fb29733dab57aa3de018f90
Rem <r3mnik@gmail.com>
2026-06-25 21:51:32 +0300

    Change person card styling to show its img in the top

--- src/components/company_person/PersonCard.js
+++ src/components/company_person/PersonCard.js
@@ -1,19 +1,28 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 function PersonCard({ record }) {
-    const avatar_url = record.avatar_url ? record.avatar_url : process.env.PUBLIC_URL + "/def_person_ava.png";
-    return (
-        <div className="min-w-md m-6 md:mb-0 col-span-12 sm:col-span-6 lg:col-span-4 border p-4 rounded-lg shadow-lg flex gap-4">
-            <img width="120" height="120" src={avatar_url} className="w-25 h-25 rounded-lg object-cover flex-shrink-0" alt="ava image" />
-
-            <div className="flex flex-col">
-                <span className="link-underline link-underline-black text-black font-bold text-lg mb-2">
-                    {record.client_name}
-                </span>
-                <p className="font-mono text-xs font-normal opacity-75 text-black mb-2">{record.role}</p>
-            </div>
-        </div>
-    );
+  const avatar_url = record.avatar_url
+    ? record.avatar_url
+    : process.env.PUBLIC_URL + "/def_person_ava.png";
+  return (
+    <div className="min-w-md m-6 md:mb-0 col-span-12 sm:col-span-6 lg:col-span-4 border p-4 rounded-lg shadow-lg flex flex-col gap-4">
+      <img
+        width="120"
+        height="120"
+        src={avatar_url}
+        className="w-25 h-25 rounded-lg object-cover self-start"
+        alt="ava image"
+      />
+      <div className="flex flex-col">
+        <span className="link-underline link-underline-black text-black font-bold text-lg mb-2">
+          {record.client_name}
+        </span>
+        <p className="font-mono text-xs font-normal opacity-75 text-black mb-2">
+          {record.role}
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default PersonCard;
