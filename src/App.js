1778fdf5a13f527fc0d277d766907b9c65a911f4
Rem <r3mnik@gmail.com>
2026-07-10 00:46:34 +0300

    Implement tags editing for compamies

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ function App() {
             <Route path="company/details/:id" element={<CompanyDetails />} />
             <Route path="company_person/create/:id" element={<CompanyDetails />} />
             <Route path="company_person/edit/:person_id/:id" element={<CompanyDetails />} />
+            <Route path="company/tags/:id" element={<CompanyDetails />} />
             <Route path="people" element={<PeopleIndex />} />
             <Route path="person/create" element={<PeopleIndex />} />
             <Route path="person/show/:id" element={<PeopleIndex />} />
---
+++ src/components/EditTags.js
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from 'react'
+import { useParams, useNavigate, Link } from 'react-router-dom'
+
+function EditTags({ tagType }) {
+    const { id } = useParams();
+    const [tags, setTags] = useState("");
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!id) return;
+        fetch(`http://localhost:3000/customers/${id}/tags`, {
+            method: 'GET',
+            headers: {
+                'content-type': 'application/json',
+                'authorization': localStorage.getItem('accessToken')
+            }
+        })
+        .then(response => {
+            if (response.ok) {
+            return response.json();
+            } else {
+            throw new Error('Failed to fetch company tags');
+            }
+        })
+        .then(data => {
+            console.log('Company Person data:', data);
+            setTags(data.join(", "));
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}, []);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setTags( value );
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const formData = new FormData();
+        formData.append('tags', tags);
+
+        fetch(`http://localhost:3000/customers/${id}/tags`, {
+            method: 'POST',
+            headers: {
+                'authorization': localStorage.getItem('accessToken')
+            },
+            body: formData
+        })
+        .then(response => {
+            if (response.ok) {
+                navigate(`/company/details/${id}`);
+            } else {
+                throw new Error('Failed to update customer tags');
+            }
+        })
+        .then(data => {
+            console.log('Customer updated:', data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    };
+
+    return (
+        <div className="p-4">
+            <h1 className="text-3xl font-bold mb-6">Tags Edit</h1>
+            <form onSubmit={handleSubmit} className="space-y-4">
+                <div>
+                    <label className="block text-gray-700 mb-1">Tags List:</label>
+                    <input type="text" name="tags" value={tags || ''} onChange={handleChange} className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" />
+                </div>
+                <button type="submit" className="bg-green-600 hover:bg-green-700 text-white font-semibold py-3 px-6 rounded">Save</button>
+                <Link to={`/company/details/${id}`} className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-3 px-6 ml-2 rounded">View</Link>
+            </form>
+        </div>
+    );    
+}
+
+export default EditTags;
