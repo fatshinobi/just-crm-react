447b1c638ef9b3f9616aa0c25f4f7b6002095e45
Rem <r3mnik@gmail.com>
2026-07-12 21:53:23 +0300

    Implement filtering by tags for people

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ function App() {
             <Route path="company_person/create/:id" element={<CompanyDetails />} />
             <Route path="company_person/edit/:person_id/:id" element={<CompanyDetails />} />
             <Route path="company/tags/:id" element={<CompanyDetails />} />
+            <Route path="people/tags/:tag" element={<PeopleIndex />} />
             <Route path="people" element={<PeopleIndex />} />
             <Route path="person/create" element={<PeopleIndex />} />
             <Route path="person/show/:id" element={<PeopleIndex />} />
