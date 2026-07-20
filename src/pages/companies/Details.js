cb5ea7537c056c33fe3e7f8887d14a1e2e0b461f
Rem <r3mnik@gmail.com>
2026-07-20 21:54:38 +0300

    Show appointments list for company

--- src/pages/companies/Details.js
+++ src/pages/companies/Details.js
@@ -2,10 +2,13 @@ import { useEffect, useState } from "react";
 import { useLocation, Link, useParams } from "react-router-dom";
 import NewCard from "../../components/NewCard";
 import ElementCard from "../../components/ElementCard";
+import AppointmentCard from "../../components/appointments/Card";
+import AppointmentCreate from "../../components/appointments/Create";
 
 function CompanyDetails() {
   const [people, setPeople] = useState([]);
   const [tags, setTags] = useState([]);
+  const [appointments, setAppointments] = useState([]);
   const location = useLocation();
   const { id } = useParams();
 
@@ -58,6 +61,31 @@ function CompanyDetails() {
         });
   }, [location.key, location.pathname]);
 
+    useEffect(() => {
+        if (!id) return;
+        fetch(`${process.env.REACT_APP_API_HOST}/customers/${id}/appointments`, {
+            method: 'GET',
+            headers: {
+                'content-type': 'application/json',
+                'authorization': localStorage.getItem('accessToken')
+            }
+        })
+        .then(response => {
+            if (response.ok) {
+            return response.json();
+            } else {
+            throw new Error('Failed to fetch company appointments');
+            }
+        })
+        .then(data => {
+            console.log('Company Appointments data:', data);
+            setAppointments(data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+  }, [location.key, location.pathname]);
+
   return (
     <div>
       <h1 className="text-4xl font-bold m-4">Company details</h1>
@@ -78,6 +106,12 @@ function CompanyDetails() {
 
       <h2 className="text-3xl font-bold m-4">Appointments</h2>
       <Link to={`/company/appointments/create/${id}`} className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-1 px-2 ml-2 rounded">Add appointment+</Link>
+      <div className="">
+        {appointments.map((record, index) => (
+          <AppointmentCard record={record} link_path={`/company_person/edit/${record.id}/${record.customer_id}`} key={index} />
+        ))}
+      </div>
+
     </div>
   );
 }
