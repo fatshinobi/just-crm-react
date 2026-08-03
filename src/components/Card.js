2c8330ed893d49efba43cb2c10919e33e28b2610
Rem <r3mnik@gmail.com>
2026-08-03 13:17:33 +0300

    Fix css styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ function Card({ record, defaultImage }) {
     const avatar_url = record.avatar_url ? record.avatar_url : process.env.PUBLIC_URL + defaultImage;
     return (
         <div className="min-w-md m-6 md:mb-0 col-span-12 sm:col-span-6 lg:col-span-4 border p-4 rounded-lg shadow-lg flex gap-4">
-            <img width="120" height="120" src={avatar_url} className="w-25 h-25 rounded-lg object-cover flex-shrink-0" alt="ava image" />
+            <img src={avatar_url} className="w-32 h-32 rounded-lg object-cover flex-shrink-0" alt="ava image" />
 
             <div className="flex flex-col">
                 <span className="link-underline link-underline-black text-black font-bold text-lg mb-2">
