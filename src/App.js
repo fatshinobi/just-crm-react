cb4b59b6946c6f530ba7843964dde4671f8aff6b
Rem <r3mnik@gmail.com>
2026-08-17 20:18:36 +0300

    Create opportunity from Person

--- src/App.js
+++ src/App.js
@@ -49,6 +49,7 @@ function App() {
             <Route path="person_company/create/:id" element={<PersonDetails />} />
             <Route path="person_company/edit/:company_id/:id" element={<PersonDetails />} />
             <Route path="person/tags/:id" element={<PersonDetails />} />
+            <Route path="person/opportunities/create/:id" element={<PersonDetails />} />
             <Route path="dashboard/appointments/show/:appointment_id" element={<Dashboards />} />
             <Route path="dashboard/appointments/edit/:appointment_id" element={<Dashboards />} />
             <Route path="dashboard/appointments/create" element={<Dashboards />} />
