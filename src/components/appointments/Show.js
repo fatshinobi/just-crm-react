5df2cd10dcde16a74b3a0a46e4f779f073da608c
Rem <r3mnik@gmail.com>
2026-08-25 12:36:02 +0300

    Default image refactoring for appointment

--- src/components/appointments/Show.js
+++ src/components/appointments/Show.js
@@ -2,24 +2,11 @@ import { useEffect, useState } from 'react'
 import { Link } from "react-router-dom"
 import { useParams } from 'react-router-dom'
 import dayjs from 'dayjs';
+import { defaultImage } from "../../constants/appointmentOptions";
 
 function AppointmentShow({isDetails}) {
     const { appointment_id } = useParams();
     const [appointment, setAppointment] = useState(null);
-
-    const defaultImage = () => {
-        switch (appointment.communication_type) {
-            case 0:
-                // task icon               
-                return "M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z";
-            case 1:
-                // email icon
-                return "M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z";
-            case 2:
-                // phone icon
-                return "M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z";
-            }
-    };
 
     const toLocalTimeView = (utcTime) => {
         if (!utcTime) return '';
@@ -56,8 +43,8 @@ function AppointmentShow({isDetails}) {
             <h1 className="text-3xl font-bold m-4">Appointment</h1>
             {appointment ? (
                 <div className="m-4 p-4 border rounded-lg shadow-lg pb-7">
-                    <svg className="w-16 h-16 md:w-20 md:h-20 mr-4 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" d={defaultImage()}></path>
+                    <svg className="w-16 h-16 md:w-20 md:h-20 mr-4 mb-4 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" d={defaultImage(appointment)}></path>
                     </svg>
 
                     <p className="mb-5"><strong>About:</strong> {appointment.about}</p>
