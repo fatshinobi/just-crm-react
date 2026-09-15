2d91f41229973221cb0b15eb64134b8ff4e70edf
Rem <r3mnik@gmail.com>
2026-09-15 15:36:08 +0300

    Implement images for attachments types

--- src/components/attachments/ElementCard.js
+++ src/components/attachments/ElementCard.js
@@ -5,13 +5,13 @@ function AttachmentElementCard({ record, link_path }) {
       //attachment_types: other: 0, image: 1, document: 2, pdf: 3
       switch (record.attachment_type) {
           case 0:
-              return "M12 8v4l3 3m-3-7a7 7 0 100 14A7 7 0 0012 8z";
+              return "M6 2h12a2 2 0 012 2v16a2 2 0 01-2 2H6a2 2 0 01-2-2V4a2 2 0 012-2z M12 17v.01 M12 14c0-1.5 1.5-2 1.5-3.25A1.75 1.75 0 0011.75 9";
           case 1:
-              return "M1 12s8 7 11 7 11-7 11-7-8-7-11-7S1 12 1 12z";
+              return "M19 3H5a2 2 0 00-2 2v14a2 2 0 002 2h14a2 2 0 002-2V5a2 2 0 00-2-2zm-3.5 5.5a.01.01 0 110-.02 M5 19l4.5-6 3.5 4.5 5-6.5 3 4";
           case 2:
-              return "M12 2l3 9h9l-6 5 2 9-7-5-7 5 2-9z";
+              return "M6 2h12a2 2 0 012 2v16a2 2 0 01-2 2H6a2 2 0 01-2-2V4a2 2 0 012-2z M8 11h8 M8 15h8";
           case 3:
-              return "M5 13l4 4L19 7";
+              return "M6 2h12a2 2 0 012 2v16a2 2 0 01-2 2H6a2 2 0 01-2-2V4a2 2 0 012-2z M4 12h8a2 2 0 012 2v1a2 2 0 01-2 2H4";
           default:
               return "M12 8v4l3 3m-3-7a7 7 0 100 14A7 7 0 0012 8z";
       }
@@ -22,11 +22,11 @@ function AttachmentElementCard({ record, link_path }) {
           case 0:
               return "text-gray-500";
           case 1:
-              return "text-blue-500";
+              return "text-purple-500";
           case 2:
-              return "text-orange-500";
+              return "text-blue-500";
           case 3:
-              return "text-green-500";
+              return "text-red-500";
           default:
               return "text-gray-500";
       }
@@ -36,7 +36,7 @@ function AttachmentElementCard({ record, link_path }) {
     <Link to={link_path} style={{ textDecoration: 'none', color: 'inherit' }} >
       <div className="border p-4 rounded-lg shadow-lg flex flex-col gap-4">
         <svg className={`w-16 h-16 md:w-20 md:h-20 mr-4 ${slassForStage(record)}`} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" d={defaultImage(record)}></path>
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d={defaultImage(record)}></path>
         </svg>
 
         <div className="flex flex-col">
